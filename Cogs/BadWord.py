f92c6cec48895fb2c75be048925f78856c23cdd6
Sooly890 <solomontnz@gmail.com>
2024-11-12 05:28:46 +0000

    change stuff

--- Cogs/BadWord.py
+++ Cogs/BadWord.py
@@ -3,7 +3,6 @@ import regex as re
 from discord.ext import commands
 from datetime import timedelta
 from Cogs import Settings, DisplayName, Utils
-from profanity_check import predict_prob
 
 def setup(bot):
 	# Add the bot and deps
@@ -20,51 +19,6 @@ class BadWord(commands.Cog):
 		DisplayName = self.bot.get_cog("DisplayName")
 
 	@commands.command(pass_context=True)
-	async def badwordenable(self, ctx):
-		"""Enables BadWord (bot-admin only)."""
-		print("badwordenable command executed")
-		if not await Utils.is_bot_admin_reply(ctx):
-			return
-		print("badwordenable command executed but admin")
-		serverOptions = self.settings.getServerStat(ctx.guild, "BadWord")
-
-		serverOptionsSet = set(serverOptions)
-		if not "enabled" in serverOptionsSet:
-			serverOptions.append("enabled")
-		
-		# Save the updated options
-		self.settings.setServerStat(ctx.guild, "BadWord", serverOptions)
-
-		await ctx.send("BadWord enabled")
-
-	@commands.command(pass_context=True)
 	async def badworddisable(self, ctx):
-		"""Disables BadWord (bot-admin only)."""
-		if not await Utils.is_bot_admin_reply(ctx):
-			return
-		serverOptions = self.settings.getServerStat(ctx.guild, "BadWord")
-
-		serverOptionsSet = set(serverOptions)
-		if "enabled" in serverOptionsSet:
-			serverOptions.remove("enabled")
-		
-		# Save the updated options
-		self.settings.setServerStat(ctx.guild, "BadWord", serverOptions)
-
+		"""Disables BadWord (bot-admin only)"""
 		await ctx.send("BadWord disabled")
-		
-	@commands.Cog.listener()
-	async def on_message(self, message):
-		print("Bad word detection triggered")
-		# Gather exclusions - no bots, no dms, and don't check if running a command
-		if message.author.bot:
-			return
-		if not message.guild:
-			return
-		print("Bad word detection triggered but admin")
-		ctx = await self.bot.get_context(message)
-		# Does the message contain a bad word?
-		prob = predict_prob([message.content])  # Predicts based on message content, not message object
-		if prob[0] > 0.5:
-			msg = 'Naughty naughty, *{}* said a bad word!'.format(DisplayName.name(ctx.author))
-			await ctx.send(msg)
